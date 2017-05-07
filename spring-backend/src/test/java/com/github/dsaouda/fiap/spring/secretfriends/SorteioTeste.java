8712c53e865dfda5accb7a9258754dc8af5d7335
Diego Saouda <dsaouda@gmail.com>
2017-05-06 22:34:22 -0300

    finalizado

--- spring-backend/src/test/java/com/github/dsaouda/fiap/spring/secretfriends/SorteioTeste.java
+++ spring-backend/src/test/java/com/github/dsaouda/fiap/spring/secretfriends/SorteioTeste.java
@@ -4,25 +4,15 @@ import javax.transaction.Transactional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.github.dsaouda.fiap.spring.secretfriends.repository.GrupoRepository;
-import com.github.dsaouda.fiap.spring.secretfriends.repository.ParticipanteRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Commit
 @Transactional
 public class SorteioTeste {
-	
-	@Autowired
-	private GrupoRepository grupoRepository;
-	
-	@Autowired
-	private ParticipanteRepository participanteRepository;
 	
 	@Test
 	public void sorteio() throws Exception {
